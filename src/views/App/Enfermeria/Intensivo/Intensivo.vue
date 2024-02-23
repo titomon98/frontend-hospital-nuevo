f21ff47b008c8623f809fe25d2e5d17760690308
Arturo Monterroso <arturomonterroso3@gmail.com>
2024-02-23 15:39:45 -0600

    Traslados completos

--- src/views/App/Enfermeria/Intensivo/Intensivo.vue
+++ src/views/App/Enfermeria/Intensivo/Intensivo.vue
@@ -10,7 +10,118 @@
     >
       <div class="iq-alert-text">{{ alertText }}</div>
     </b-alert>
-    <b-modal id="modal-2-expediente" ref="modal-2-expediente" title="Editar expediente">
+    <b-modal id="modal-traslado" ref="modal-traslado" title="Trasladar paciente">
+      <b-alert
+        :show="alertCountDownError"
+        dismissible
+        fade
+        @dismissed="alertCountDownError=0"
+        class="text-white bg-danger"
+      >
+        <div class="iq-alert-text">{{ alertErrorText }}</div>
+      </b-alert>
+      <b-form @submit="$event.preventDefault()">
+        <b-col >
+          <b-form-group label="Área a la que desea trasladar:">
+            <b-form-radio
+              v-model="form.selectedOption"
+              value="hospi"
+              name="customRadio1"
+            >Hospitalización</b-form-radio>
+            <b-form-radio
+              v-model="form.selectedOption"
+              value="emergencia"
+              name="customRadio1"
+            >Emergencia</b-form-radio>
+            <b-form-radio
+              v-model="form.selectedOption"
+              value="quirofano"
+              name="customRadio1"
+            >Quirófano</b-form-radio>
+          </b-form-group>
+        </b-col>
+      </b-form>
+      <template #modal-footer="{}">
+        <b-button variant="primary" @click="saveTraslado('traslado')"
+          >Guardar</b-button
+        >
+        <b-button variant="danger" @click="closeModal('traslado')"
+          >Cancelar</b-button
+        >
+      </template>
+    </b-modal>
+    <b-modal size="lg" id="modal-add-receta" ref="modal-add-receta" title="Contenido de receta">
+      <b-alert
+        :show="alertCountDownError"
+        dismissible
+        fade
+        @dismissed="alertCountDownError=0"
+        class="text-white bg-danger"
+      >
+        <div class="iq-alert-text">{{ alertErrorText }}</div>
+      </b-alert>
+      <b-form @submit="$event.preventDefault()">
+        <b-form-group label="Contenido:">
+          <quill-editor v-model="form.receta" :options="editorOptions" class="custom-editor"></quill-editor>
+        </b-form-group>
+      </b-form>
+      <template #modal-footer="{}">
+        <b-button variant="primary" @click="saveReceta()"
+          >Guardar</b-button
+        >
+        <b-button variant="danger" @click="closeModal('add-receta')"
+          >Cancelar</b-button
+        >
+      </template>
+    </b-modal>
+    <b-modal id="modal-ver-receta" ref="modal-ver-receta" title="Ver recetas">
+      <b-alert
+        :show="alertCountDownError"
+        dismissible
+        fade
+        @dismissed="alertCountDownError=0"
+        class="text-white bg-danger"
+      >
+        <div class="iq-alert-text">{{ alertErrorText }}</div>
+      </b-alert>
+      <vuetable
+          ref="vuetableRecetas"
+          class="table-divided table-responsive order-with-arrow"
+          :api-url="apiBaseReceta"
+          :query-params="makeQueryParamsReceta"
+          :per-page="perPage"
+          :reactive-api-url="true"
+          :fields="fieldsReceta"
+          pagination-path
+          @vuetable:pagination-data="onPaginationDataReceta"
+        >
+          <template slot="actions" slot-scope="props">
+            <b-button-group>
+              <b-button
+                v-b-tooltip.top="'Generar voucher de pago'"
+                @click="voucherData(props.rowData)"
+                class="mb-2"
+                size="sm"
+                variant="outline-info"
+                ><i :class="'fas fa-money-bill'"
+              /></b-button>
+            </b-button-group>
+          </template>
+        </vuetable>
+        <vuetable-pagination-bootstrap
+          ref="paginationReceta"
+          @vuetable-pagination:change-page="onChangePageReceta"
+        />
+      <template #modal-footer="{}">
+        <b-button variant="primary" @click="onValidate('ver-receta')"
+          >Guardar</b-button
+        >
+        <b-button variant="danger" @click="closeModal('ver-receta')"
+          >Cancelar</b-button
+        >
+      </template>
+    </b-modal>
+    <b-modal id="modal-add-servicio" ref="modal-add-servicio" title="Añadir servicio">
       <b-alert
         :show="alertCountDownError"
         dismissible
@@ -33,10 +144,41 @@
         </b-form-group>
       </b-form>
       <template #modal-footer="{}">
-        <b-button variant="primary" @click="onValidate('update')"
+        <b-button variant="primary" @click="onValidate('add-servicio')"
           >Guardar</b-button
         >
-        <b-button variant="danger" @click="closeModal('update')"
+        <b-button variant="danger" @click="closeModal('add-servicio')"
+          >Cancelar</b-button
+        >
+      </template>
+    </b-modal>
+    <b-modal id="modal-ver-servicio" ref="modal-ver-servicio" title="Ver servicios">
+      <b-alert
+        :show="alertCountDownError"
+        dismissible
+        fade
+        @dismissed="alertCountDownError=0"
+        class="text-white bg-danger"
+      >
+        <div class="iq-alert-text">{{ alertErrorText }}</div>
+      </b-alert>
+      <b-form @submit="$event.preventDefault()">
+        <b-form-group label="Nombre:">
+          <b-form-input
+            v-model.trim="$v.form.name.$model"
+            :state="!$v.form.name.$error"
+            placeholder="Ingresar nombre de banco"
+          ></b-form-input>
+          <div v-if="$v.form.name.required.$invalid" class="invalid-feedback">
+            Debe ingresar el nombre
+          </div>
+        </b-form-group>
+      </b-form>
+      <template #modal-footer="{}">
+        <b-button variant="primary" @click="onValidate('ver-servicio')"
+          >Guardar</b-button
+        >
+        <b-button variant="danger" @click="closeModal('ver-servicio')"
           >Cancelar</b-button
         >
       </template>
@@ -76,43 +218,48 @@
               pagination-path
               @vuetable:pagination-data="onPaginationData"
             >
-              <!-- Estado -->
-              <!-- Estado -->
-              <div slot="estado" slot-scope="props">
-                <button v-if="props.rowData.estado === 1" type="button" class="btn btn-success" disabled>INGRESADO EN HOSPITAL</button>
-                <button v-if="props.rowData.estado === 2" type="button" class="btn btn-warning" disabled>EGRESADO</button>
-                <button v-if="props.rowData.estado === 3" type="button" class="btn btn-success" disabled>INGRESADO EN QUIRÓFANO</button>
-                <button v-if="props.rowData.estado === 4" type="button" class="btn btn-success" disabled>INGRESADO EN INTENSIVO</button>
-                <button v-if="props.rowData.estado === 5" type="button" class="btn btn-success" disabled>INGRESADO EN EMERGENCIA</button>
-                <button v-if="props.rowData.estado === 0" type="button" class="btn btn-dark" disabled>FALLECIDO</button>
-              </div>
               <!-- Botones -->
               <template slot="actions" slot-scope="props">
                 <b-button-group>
                   <b-button
-                    v-b-tooltip.top="'Editar'"
-                    @click="setData(props.rowData)"
-                    v-b-modal.modal-2-expediente
-                    class="mb-2"
-                    size="sm"
-                    variant="outline-warning"
-                    ><i :class="'fas fa-pencil-alt'"
-                  /></b-button>
-                  <b-button
                     v-b-tooltip.top="'Trasladar'"
-                    @click="setData(props.rowData)"
+                    @click="traslado(props.rowData.id)"
                     class="mb-2"
                     size="sm"
-                    variant="outline-success"
+                    variant="outline-primary"
                     ><i :class="'fas fa-heart'"
                   /></b-button>
                   <b-button
-                    v-b-tooltip.top="'Detalle de cuentas previas'"
-                    @click="setData(props.rowData)"
+                    v-b-tooltip.top="'Agregar receta'"
+                    @click="addReceta(props.rowData.id)"
                     class="mb-2"
                     size="sm"
                     variant="outline-dark"
                     ><i :class="'fas fa-list-alt'"
+                  /></b-button>
+                  <b-button
+                    v-b-tooltip.top="'Ver recetas'"
+                    @click="verReceta(props.rowData.id)"
+                    class="mb-2"
+                    size="sm"
+                    variant="outline-warning"
+                    ><i :class="'fas fa-eye'"
+                  /></b-button>
+                  <b-button
+                    v-b-tooltip.top="'Agregar servicios'"
+                    @click="addServicio(props.rowData.id)"
+                    class="mb-2"
+                    size="sm"
+                    variant="outline-success"
+                    ><i :class="'fas fa-list-alt'"
+                  /></b-button>
+                  <b-button
+                    v-b-tooltip.top="'Ver servicios'"
+                    @click="verServicio(props.rowData.id)"
+                    class="mb-2"
+                    size="sm"
+                    variant="outline-secondary"
+                    ><i :class="'fas fa-eye'"
                   /></b-button>
                 </b-button-group>
               </template>
@@ -137,13 +284,15 @@ import useVuelidate from '@vuelidate/core'
 import { required } from '@vuelidate/validators'
 import axios from 'axios'
 import { apiUrl } from '../../../../config/constant'
+import { quillEditor } from 'vue-quill-editor'
 
 export default {
   name: 'Bank',
   components: {
     vuetable: Vuetable,
     'vuetable-pagination-bootstrap': VuetablePaginationBootstrap,
-    'datatable-heading': DatatableHeading
+    'datatable-heading': DatatableHeading,
+    quillEditor
   },
   setup () {
     return { $v: useVuelidate() }
@@ -156,12 +305,27 @@ export default {
       from: 0,
       to: 0,
       total: 0,
+      editorOptions: {
+        modules: {
+          toolbar: [
+            [{ 'header': '1' }, { 'header': '2' }, { 'font': [] }],
+            [{ size: [] }],
+            ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+            [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+            ['clean']
+          ]
+        },
+        placeholder: 'Escribir contenido de la receta',
+        theme: 'snow'
+      },
       perPage: 5,
       search: '',
       form: {
         id: 0,
         name: '',
-        state: 1
+        state: 1,
+        selectedOption: 'hospi',
+        receta: null
       },
       alertSecs: 5,
       alertCountDown: 0,
@@ -218,6 +382,38 @@ export default {
           sortField: 'contacto_encargado',
           title: 'Contacto de encargado',
           dataClass: 'list-item-heading'
+        }
+      ],
+      fieldsReceta: [
+        {
+          name: 'contenido',
+          sortField: 'contenido',
+          title: 'Indicaciones',
+          dataClass: 'list-item-heading'
+        },
+        {
+          name: 'tipo',
+          sortField: 'tipo',
+          title: 'Tipo',
+          dataClass: 'list-item-heading'
+        },
+        {
+          name: 'tipo_movimiento.nombre',
+          sortField: 'tipo_movimiento.nombre',
+          title: 'Tipo de movimiento',
+          dataClass: 'list-item-heading'
+        },
+        {
+          name: 'cantidad',
+          sortField: 'cantidad',
+          title: 'Cantidad',
+          dataClass: 'list-item-heading'
+        },
+        {
+          name: 'cantidad_previa',
+          sortField: 'cantidad_previa',
+          title: 'Cantidad previa',
+          dataClass: 'list-item-heading'
         },
         {
           name: '__slot:estado',
@@ -250,9 +446,41 @@ export default {
     },
     closeModal (action) {
       switch (action) {
-        case 'update': {
+        case 'traslado': {
           this.$v.$reset()
-          this.$refs['modal-2-expediente'].hide()
+          this.$refs['modal-traslado'].hide()
+          this.form.id = 0
+          this.form.name = ''
+          this.form.state = 1
+          break
+        }
+        case 'add-receta': {
+          this.$v.$reset()
+          this.$refs['modal-add-receta'].hide()
+          this.form.id = 0
+          this.form.name = ''
+          this.form.state = 1
+          break
+        }
+        case 'ver-receta': {
+          this.$v.$reset()
+          this.$refs['modal-ver-receta'].hide()
+          this.form.id = 0
+          this.form.name = ''
+          this.form.state = 1
+          break
+        }
+        case 'add-servicio': {
+          this.$v.$reset()
+          this.$refs['modal-add-servicio'].hide()
+          this.form.id = 0
+          this.form.name = ''
+          this.form.state = 1
+          break
+        }
+        case 'ver-servicio': {
+          this.$v.$reset()
+          this.$refs['modal-ver-servicio'].hide()
           this.form.id = 0
           this.form.name = ''
           this.form.state = 1
@@ -273,15 +501,67 @@ export default {
         this.showAlertError()
       }
     },
-    setData (data) {
-      this.form.name = data.nombre
-      this.form.state = data.estado
-      this.form.id = data.id
+    traslado (id) {
+      this.$refs['modal-traslado'].show()
+      this.form.id = id
+    },
+    saveTraslado () {
+      const me = this
+      axios.put(apiUrl + '/expedientes/changeStatus', {
+        form: me.form })
+        .then((response) => {
+          me.alertVariant = 'primary'
+          me.showAlert()
+          me.alertText = 'Se ha trasladado el expediente ' + me.form.nombre + ' exitosamente'
+          me.$refs.vuetable.refresh()
+          me.closeModal('traslado')
+          me.form.id = 0
+          me.form.selectedOption = 'hospi'
+        })
+        .catch((error) => {
+          me.alertVariant = 'danger'
+          me.showAlertError()
+          me.alertErrorText = 'Ha ocurrido un error, por favor intente más tarde'
+          console.error('Error!', error)
+        })
+    },
+    addReceta (id) {
+      this.$refs['modal-add-receta'].show()
+      this.form.id = id
+    },
+    saveReceta () {
+      const me = this
+      if (me.form.receta !== null) {
+        axios.post(apiUrl + '/recetas/create', {
+          form: me.form })
+          .then((response) => {
+            me.alertVariant = 'primary'
+            me.showAlert()
+            me.alertText = 'Se ha creado la receta exitosamente'
+            me.$refs.vuetable.refresh()
+            me.closeModal('add-receta')
+            me.form.id = 0
+          })
+          .catch((error) => {
+            me.alertVariant = 'danger'
+            me.showAlertError()
+            me.alertErrorText = 'Ha ocurrido un error, por favor intente más tarde'
+            console.error('Error!', error)
+          })
+      }
+    },
+    verReceta (id) {
+      this.$refs['modal-ver-receta'].show()
+    },
+    addServicio (id) {
+      this.$refs['modal-add-servicio'].show()
+    },
+    verServicio (id) {
+      this.$refs['modal-ver-servicio'].show()
     },
     /* Guardar */
     onUpdate () {
       const me = this
-      // this.$refs["modalSave"].hide();
       axios.put(apiUrl + '/expedientes/update', {
         form: me.form })
         .then((response) => {
@@ -339,7 +619,51 @@ export default {
     },
     showAlertError () {
       this.alertCountDownError = this.alertSecs
+    },
+    makeQueryParamsReceta (sortOrder, currentPage, perPage) {
+      return sortOrder[0]
+        ? {
+          criterio: sortOrder[0] ? sortOrder[0].sortField : 'createdAt',
+          order: sortOrder[0] ? sortOrder[0].direction : 'desc',
+          page: currentPage,
+          limit: this.perPage
+        }
+        : {
+          criterio: sortOrder[0] ? sortOrder[0].sortField : 'createdAt',
+          order: sortOrder[0] ? sortOrder[0].direction : 'desc',
+          page: currentPage,
+          limit: this.perPage
+        }
+    },
+    onPaginationDataReceta (paginationData) {
+      this.fromP = paginationData.from
+      this.toP = paginationData.to
+      this.totalP = paginationData.total
+      this.lastPageP = paginationData.last_page
+      this.items = paginationData.data
+      this.$refs.paginationReceta.setPaginationData(paginationData)
+    },
+    onChangePageReceta (page) {
+      this.$refs.vuetableRecetas.changePage(page)
+    },
+    getDataRecetas (data) {
+      this.form.name = data.nombre
+      this.form.number = data.numero_cuenta_bancaria
+      this.form.quantity = data.cantidad
+      this.form.bank_id = data.banco.nombre
+      this.form.state = data.estado
+      this.form.id = data.id
+      let id = data.id
+      this.apiBaseReceta = apiUrl + `/recetas/getId?id=${id}`
     }
   }
 }
 </script>
+<style>
+.custom-editor {
+  height: 500px; /* Adjust the height as needed */
+}
+.custom-editor .ql-editor {
+  color: #333; /* Adjust the color value to make the text darker */
+}
+</style>
