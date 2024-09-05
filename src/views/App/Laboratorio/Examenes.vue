7633206ef099bfdb97f137ad1a54bf57787f1f82
DevJos404 <lopez.dev404@gmail.com>
2024-09-05 13:14:38 -0600

    Correccion de vistas en examenes

--- src/views/App/Laboratorio/Examenes.vue
+++ src/views/App/Laboratorio/Examenes.vue
@@ -306,7 +306,7 @@
             <b-button variant="primary"  v-b-modal.modal_agregar>AGREGAR NUEVO</b-button>
           </template>
           <template v-slot:body>
-            <h4 class="card-title">EXAMENES SOLICITADOS</h4>
+            <h4 class="card-title">EXAMENES SOLICITADOS  Y PENDIENTES DE PAGO</h4>
             <div class="row mb-3">
               <div class="col-md-4">
                 <b-form-group label="Fecha Desde:">
@@ -387,7 +387,7 @@
       <b-col sm="12">
         <iq-card>
           <template v-slot:body>
-            <h4 class="card-title">EXAMENES PENDIENTES DE PAGO</h4>
+            <h4 class="card-title">EXAMENES PAGADOS Y PENDIENTES DE RESULTADO</h4>
             <div class="row mb-3">
               <div class="col-md-4">
                 <b-form-group label="Fecha Desde:">
@@ -404,64 +404,48 @@
               </div>
             </div>
             <datatable-heading
-              :changePageSize="changePageSizes"
-              :searchChange="searchChange"
-              :from="from"
-              :to="to"
-              :total="total"
-              :perPage="perPage"
+              :changePageSize="changePageSizes2"
+              :searchChange="searchChange2"
+              :from="from2"
+              :to="to2"
+              :total="total2"
+              :perPage="perPage2"
             >
             </datatable-heading>
             <vuetable
-              ref="vuetable"
+              ref="vuetable2"
               class="table-divided order-with-arrow"
-              :api-url="apiBase"
-              :query-params="makeQueryParams"
-              :per-page="perPage"
+              :api-url="apiBase2"
+              :query-params="makeQueryParams2"
+              :per-page="perPage2"
               :reactive-api-url="true"
-              :fields="fields"
+              :fields="fields2"
               pagination-path
-              @vuetable:pagination-data="onPaginationData"
+              @vuetable:pagination-data="onPaginationData2"
             >
             <!-- Botones -->
             <template slot="actions" slot-scope="props">
               <b-button-group>
                 <div class="button-container">
-                  <!-- <b-button
+                 <b-button
                     @click="addResultado(props.rowData.id, props.rowData.id_examenes_almacenados)"
                     class="mb-2 button-spacing"
                     size="sm"
                     variant="success"
                   >Agregar resultado</b-button>
-
-                  <b-button
-                    @click="verResultado(props.rowData.id)"
-                    class="mb-2 button-spacing"
-                    size="sm"
-                    variant="dark"
-                  >Ver resultado</b-button>
-
-                  <b-button
-                    @click="ImprimirResultado(props.rowData.id)"
-                    class="mb-2 button-spacing"
-                    size="sm"
-                    variant="success"
-                  >Imprimir Resultado</b-button> -->
-
                   <b-button
                   @click="mostrarConfirmacionAnulacion(props.rowData.id)"
-                  class="mb-2"
+                    class="mb-2 button-spacing"
                     size="sm"
-                    variant="outline-warning"
-                    ><i :class="'fas fa-money'"
-                  />Pagar</b-button>
+                    variant="danger"
+                  >Anular Examen</b-button>
                 </div>
               </b-button-group>
             </template>
             </vuetable>
             <vuetable-pagination-bootstrap
-                ref="pagination"
-                @vuetable-pagination:change-page="onChangePage"
+                ref="pagination2"
+                @vuetable-pagination:change-page="onChangePage2"
               />
           </template>
         </iq-card>
@@ -469,7 +453,7 @@
       <b-col sm="12">
         <iq-card>
           <template v-slot:body>
-            <h4 class="card-title">AGREGAR RESULTADOS</h4>
+            <h4 class="card-title">EXAMENES REALIZADOS (FINALIZADOS)</h4>
             <div class="row mb-3">
               <div class="col-md-4">
                 <b-form-group label="Fecha Desde:">
@@ -486,36 +470,29 @@
               </div>
             </div>
             <datatable-heading
-              :changePageSize="changePageSizes"
-              :searchChange="searchChange"
-              :from="from"
-              :to="to"
-              :total="total"
-              :perPage="perPage"
+              :changePageSize="changePageSizes3"
+              :searchChange="searchChange3"
+              :from="from3"
+              :to="to3"
+              :total="total3"
+              :perPage="perPage3"
             >
             </datatable-heading>
             <vuetable
-              ref="vuetable"
+              ref="vuetable3"
               class="table-divided order-with-arrow"
-              :api-url="apiBase"
-              :query-params="makeQueryParams"
-              :per-page="perPage"
+              :api-url="apiBase3"
+              :query-params="makeQueryParams3"
+              :per-page="perPage3"
               :reactive-api-url="true"
-              :fields="fields"
+              :fields="fields3"
               pagination-path
-              @vuetable:pagination-data="onPaginationData"
+              @vuetable:pagination-data="onPaginationData3"
             >
             <!-- Botones -->
             <template slot="actions" slot-scope="props">
               <b-button-group>
                 <div class="button-container">
-                  <b-button
-                    @click="addResultado(props.rowData.id, props.rowData.id_examenes_almacenados)"
-                    class="mb-2 button-spacing"
-                    size="sm"
-                    variant="success"
-                  >Agregar resultado</b-button>
-
                   <b-button
                     @click="verResultado(props.rowData.id)"
                     class="mb-2 button-spacing"
@@ -529,20 +506,13 @@
                     size="sm"
                     variant="success"
                   >Imprimir Resultado</b-button>
-
-                  <b-button
-                  @click="mostrarConfirmacionAnulacion(props.rowData.id)"
-                    class="mb-2 button-spacing"
-                    size="sm"
-                    variant="danger"
-                  >Anular Examen</b-button>
                 </div>
               </b-button-group>
               </template>
             </vuetable>
             <vuetable-pagination-bootstrap
-                ref="pagination"
-                @vuetable-pagination:change-page="onChangePage"
+                ref="pagination3"
+                @vuetable-pagination:change-page="onChangePage3"
               />
           </template>
         </iq-card>
@@ -681,6 +651,18 @@ export default {
       fechaDesde: null,
       fechaHasta: null,
       apiBase: apiUrl + '/Examenes_realizados/list',
+      apiBase2: apiUrl + '/Examenes_realizados/list2',
+      apiBase3: apiUrl + '/Examenes_realizados/list3',
+      from2: 0,
+      to2: 0,
+      total2: 0,
+      perPage2: 5,
+      search2: '',
+      from3: 0,
+      to3: 0,
+      total3: 0,
+      perPage3: 5,
+      search3: '',
       slickOptions: {
         centerMode: false,
         centerPadding: '60px',
@@ -758,6 +740,112 @@ export default {
           name: 'por_pagar',
           sortField: 'por_pagar',
           title: 'Por Pagar',
+          dataClass: 'list-item-heading'
+        },
+        {
+          name: 'nombre_examen',
+          sortField: 'nombre_examen',
+          title: 'Examen Realizado',
+          dataClass: 'list-item-heading'
+        },
+        {
+          name: 'fecha_hora',
+          sortField: 'fecha_hora',
+          title: 'Fecha y Hora',
+          dataClass: 'list-item-heading'
+        }
+      ],
+      fields2: [
+        {
+          name: '__slot:actions',
+          title: 'Acciones',
+          titleClass: '',
+          dataClass: 'text-muted'
+        },
+        {
+          name: 'nombre',
+          sortField: 'nombre',
+          title: 'Nombre',
+          dataClass: 'list-item-heading'
+        },
+        {
+          name: 'cui',
+          sortField: 'cui',
+          title: 'CUI',
+          dataClass: 'list-item-heading'
+        },
+        {
+          name: 'total',
+          sortField: 'total',
+          title: 'Total a Pagar',
+          dataClass: 'list-item-heading'
+        },
+        {
+          name: 'whatsapp',
+          sortField: 'whatsapp',
+          title: 'Celular',
+          dataClass: 'list-item-heading'
+        },
+        {
+          name: 'numero_muestra',
+          sortField: 'numero_muestra',
+          title: 'Numero de Muestra',
+          dataClass: 'list-item-heading'
+        },
+        {
+          name: 'nombre_encargago',
+          sortField: 'nombre_encargago',
+          title: 'Nombre de Encargado',
+          dataClass: 'list-item-heading'
+        },
+        {
+          name: 'pagado',
+          sortField: 'pagado',
+          title: 'Pagado',
+          dataClass: 'list-item-heading'
+        },
+        {
+          name: 'por_pagar',
+          sortField: 'por_pagar',
+          title: 'Por Pagar',
+          dataClass: 'list-item-heading'
+        },
+        {
+          name: 'nombre_examen',
+          sortField: 'nombre_examen',
+          title: 'Examen Realizado',
+          dataClass: 'list-item-heading'
+        },
+        {
+          name: 'fecha_hora',
+          sortField: 'fecha_hora',
+          title: 'Fecha y Hora',
+          dataClass: 'list-item-heading'
+        }
+      ],
+      fields3: [
+        {
+          name: '__slot:actions',
+          title: 'Acciones',
+          titleClass: '',
+          dataClass: 'text-muted'
+        },
+        {
+          name: 'nombre',
+          sortField: 'nombre',
+          title: 'Nombre',
+          dataClass: 'list-item-heading'
+        },
+        {
+          name: 'whatsapp',
+          sortField: 'whatsapp',
+          title: 'Celular',
+          dataClass: 'list-item-heading'
+        },
+        {
+          name: 'numero_muestra',
+          sortField: 'numero_muestra',
+          title: 'Numero de Muestra',
           dataClass: 'list-item-heading'
         },
         {
@@ -1058,6 +1146,77 @@ export default {
     },
     onChangePage (page) {
       this.$refs.vuetable.changePage(page)
+    },
+
+    makeQueryParams2 (sortOrder2, currentPage2, perPage2) {
+      return {
+        criterio: sortOrder2[0] ? sortOrder2[0].sortField : 'createdAt',
+        order: sortOrder2[0] ? sortOrder2[0].direction : 'desc',
+        page: currentPage2,
+        limit: this.perPage2,
+        search: this.search2,
+        fechaDesde: this.fechaDesde ? moment(this.fechaDesde).format('YYYY-MM-DD') : null,
+        fechaHasta: this.fechaHasta ? moment(this.fechaHasta).format('YYYY-MM-DD') : null
+      }
+    },
+    changePageSizes2 (perPage) {
+      this.perPage2 = perPage
+      this.$refs.vuetable2.refresh()
+    },
+    searchChange2 (val) {
+      this.search2 = val.toLowerCase()
+      this.$refs.vuetable2.refresh()
+    },
+    onPaginationData2 (paginationData) {
+      this.from2 = paginationData.from
+      this.to2 = paginationData.to
+      this.total2 = paginationData.total
+      this.lastPage2 = paginationData.last_page
+      this.items2 = paginationData.data.map(item => {
+        item.fecha_hora = moment(item.fecha_hora).format('DD/MM/YYYY HH:mm')
+        return {
+          fecha_hora: item.fecha_hora
+        }
+      })
+      this.$refs.pagination2.setPaginationData(paginationData)
+    },
+    onChangePage2 (page) {
+      this.$refs.vuetable2.changePage(page)
+    },
+    makeQueryParams3 (sortOrder3, currentPage3, perPage3) {
+      return {
+        criterio: sortOrder3[0] ? sortOrder3[0].sortField : 'createdAt',
+        order: sortOrder3[0] ? sortOrder3[0].direction : 'desc',
+        page: currentPage3,
+        limit: this.perPage3,
+        search: this.search3,
+        fechaDesde: this.fechaDesde ? moment(this.fechaDesde).format('YYYY-MM-DD') : null,
+        fechaHasta: this.fechaHasta ? moment(this.fechaHasta).format('YYYY-MM-DD') : null
+      }
+    },
+    changePageSizes3 (perPage) {
+      this.perPage3 = perPage
+      this.$refs.vuetable3.refresh()
+    },
+    searchChange3 (val) {
+      this.search3 = val.toLowerCase()
+      this.$refs.vuetable3.refresh()
+    },
+    onPaginationData3 (paginationData) {
+      this.from3 = paginationData.from
+      this.to3 = paginationData.to
+      this.total3 = paginationData.total
+      this.lastPage3 = paginationData.last_page
+      this.items3 = paginationData.data.map(item => {
+        item.fecha_hora = moment(item.fecha_hora).format('DD/MM/YYYY HH:mm')
+        return {
+          fecha_hora: item.fecha_hora
+        }
+      })
+      this.$refs.pagination3.setPaginationData(paginationData)
+    },
+    onChangePage3 (page) {
+      this.$refs.vuetable3.changePage(page)
     },
 
     /* AREA PARA AGREGAR, VER E IMPRIMIR RESULTADOS */
