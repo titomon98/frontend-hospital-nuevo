c5b9ef4d8076bdfa83722414a6af8761ee921bec
dabo1103 <batz.dev.404@gmail.com>
2025-05-22 00:43:18 -0600

    Add Costo_Quimioterapia

--- src/views/App/Enfermeria/Ingresos/IngresosEnfermeria.vue
+++ src/views/App/Enfermeria/Ingresos/IngresosEnfermeria.vue
@@ -97,14 +97,22 @@
                   <div v-for="(tipo, index) in tiposHabitaciones" :key="tipo">
                     <b-row>
                       <b-col cols="4">
-                        <h6 class="mt-2">{{ NombreHabitaciones[index] }}</h6>
+                        <h6 v-if="(form.estudioDeSueño != 1 && form.estudioDeSueño != 2) || index == 2" class="mt-2">{{ NombreHabitaciones[index] }}</h6>
                         <b-form-checkbox
-                          v-if="index === 0"
+                          v-if="this.selectedOption == 'hospi'"
                           v-model="form.estudioDeSueño"
                           value="1"
                           class="mt-2 mb-4"
                         >
                           Estudio de sueño
+                        </b-form-checkbox>
+                        <b-form-checkbox
+                          v-if="this.selectedOption == 'hospi'"
+                          v-model="form.estudioDeSueño"
+                          value="2"
+                          class="mt-2 mb-4"
+                        >
+                          Quimioterapia
                         </b-form-checkbox>
                       </b-col>
                       <b-col
